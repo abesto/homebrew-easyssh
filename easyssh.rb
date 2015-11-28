c876fb8a2a255f78090754f1aab1241a48250f40
Zoltan Nagy <abesto@abesto.net>
2015-11-28 11:17:55 +0100

    Release 0.1.1-alpha.1

--- easyssh.rb
+++ easyssh.rb
@@ -4,9 +4,9 @@ class Easyssh < Formula
   desc "SSH-related aliases done right"
   homepage 'https://github.com/abesto/easyssh'
   head 'git@github.com:abesto/easyssh', :using => :git
-  version '0.1'
+  version '0.1.1-alpha.1'
   url "https://github.com/abesto/easyssh/archive/v#{version}.tar.gz"
-  sha256 "842fda46e9b13a8a3e04c539882ae3a16bd9d765616ffe708eb4a35b651e7e01"
+  sha256 "def7ea1117c64492fe5bff82ac642e3d145efccd5996c38e8f4d38dcb59768d8"
 
   depends_on "go" => :build
   depends_on "godep" => :build
