e648746730d38b17c7d8cce7bb787b28d6c46a13
Zoltan Nagy <abesto@abesto.net>
2016-11-18 23:26:53 +0100

    Bump to 0.2.1; Correctly set version and build date

--- easyssh.rb
+++ easyssh.rb
@@ -1,21 +1,25 @@
 require "language/go"
+require "time"
 
 class Easyssh < Formula
   desc "SSH-related aliases done right"
   homepage 'https://github.com/abesto/easyssh'
   head 'git@github.com:abesto/easyssh', :using => :git
-  version '0.1.2'
+  version '0.2.1'
   url "https://github.com/abesto/easyssh/archive/v#{version}.tar.gz"
-  sha256 "77d59a7f7ec3369b0dc4737b194e97f232dc669f94130827973f7cf398ccb623"
+  sha256 "f12fabf614ca9273560f5735f2caa594013fc8709be6a78e38c9f8f8361474ff"
 
   depends_on "go" => :build
-  depends_on "godep" => :build
 
   def install
     ENV["GOPATH"] = buildpath
-    mkdir_p buildpath/"src/github.com/abesto/"
-    ln_sf buildpath, buildpath/"src/github.com/abesto/easyssh"
+    deep = buildpath/"src/github.com/abesto/easyssh"
+    mkdir_p deep
+    mv Dir['*'] - ['src'], deep
 
-    system "godep", "go", "build", "-o", bin/"easyssh"
+    Dir.chdir(deep) {
+      system "go", "build", "-o", bin/"easyssh",
+             '-ldflags', "-X main.VERSION=#{version} -X main.BUILD_DATE=#{Time.now.utc.iso8601}"
+    }
   end
 end
